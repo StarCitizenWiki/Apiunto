62d2666b1da970466157ecde51f9abd8274309f5
FoXFTW <hannes@octofox.de>
2018-08-18 21:03:59 +0200

    :tada: Initial Commit

---
+++ src/Repositories/Vehicle/GroundVehicleRepository.php
@@ -0,0 +1,14 @@
+<?php declare( strict_types=1 );
+/**
+ * User: Hannes
+ * Date: 18.08.2018
+ * Time: 12:04
+ */
+
+namespace MediaWiki\Extension\Apiunto\Repositories\Vehicle;
+
+class GroundVehicleRepository extends AbstractVehicleRepository {
+
+	const API_ENDPOINT = 'api/vehicles/ground_vehicles';
+
+}
---
+++ src/mw.ext.Apiunto.lua
@@ -0,0 +1,57 @@
+local libraryUtil = require('libraryUtil')
+local Apiunto = {}
+local php
+
+function Apiunto.get_ship(name, locale)
+    libraryUtil.checkType('get_ship', 1, name, 'string', false)
+    libraryUtil.checkTypeMulti('get_ship', 2, locale, { 'string', 'nil' })
+
+    name = tostring(name)
+    locale = getLocale(locale)
+
+    return php.get_ship(name, locale)
+end
+
+function Apiunto.get_ground_vehicle(name, locale)
+    libraryUtil.checkType('get_ground_vehicle', 1, name, 'string', false)
+    libraryUtil.checkTypeMulti('get_ground_vehicle', 2, locale, { 'string', 'nil' })
+
+    name = tostring(name)
+    locale = getLocale(locale)
+
+    return php.get_ground_vehicle(name, locale)
+end
+
+function Apiunto.get_manufacturer(name, locale)
+    libraryUtil.checkType('get_manufacturer', 1, name, 'string', false)
+    libraryUtil.checkTypeMulti('get_manufacturer', 2, locale, { 'string', 'nil' })
+
+    name = tostring(name)
+    locale = getLocale(locale)
+
+    return php.get_manufacturer(name, locale)
+end
+
+function Apiunto.setupInterface(options)
+    -- Boilerplate
+    Apiunto.setupInterface = nil
+    php = mw_interface
+    mw_interface = nil
+
+    -- Register this library in the "mw" global
+    mw = mw or {}
+    mw.ext = mw.ext or {}
+    mw.ext.Apiunto = Apiunto
+
+    package.loaded['mw.ext.Apiunto'] = Apiunto
+end
+
+function getLocale(locale)
+    if locale == nil then
+        return ''
+    else
+        return tostring(locale)
+    end
+end
+
+return Apiunto
