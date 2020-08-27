b057dc3a13e8832be47a6e5acb1f8203fc4079b8
Hannes <hannes@octofox.de>
2020-08-27 14:25:39 +0200

    :bug: Fix catching non-imported classes

--- src/Repositories/CommLinkRepository.php
+++ src/Repositories/CommLinkRepository.php
@@ -1,31 +1,36 @@
 <?php declare(strict_types = 1);
-/**
- * User: Hannes
- * Date: 11.10.2018
- * Time: 13:57
- */
 
 namespace MediaWiki\Extension\Apiunto\Repositories;
 
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+
+/**
+ * Star Citizen Comm-Link metadata
+ */
 class CommLinkRepository extends AbstractRepository
 {
     const API_ENDPOINT = 'api/comm-links';
 
     /**
+     * Comm-Link Metadata
+     * https://docs.star-citizen.wiki/star_citizen_api.html#einzelner-comm-link
+     *
      * @return string
      */
-    public function getCommLinkMetadata() {
+    public function getCommLinkMetadata()
+    {
         try {
             $response = $this->client->get( $this->makeUrl() );
-        }
-        catch ( ConnectException  $e ) {
-            return \GuzzleHttp\json_encode( [
+        } catch ( ConnectException $e ) {
+            return \GuzzleHttp\json_encode(
+                [
                 'error' => 500,
                 'message' => 'Could not connect to Api',
-            ] );
-        }
-        catch ( ClientException $e ) {
-            return $e->getResponse()->getBody()->getContents();
+                 ] 
+            );
+        } catch ( ClientException $e ) {
+            return $this->responseFromException( $e );
         }
 
         return $response->getBody()->getContents();
