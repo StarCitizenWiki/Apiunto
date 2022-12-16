541a9f4d4907689a2644c6e9492990ae9fb11089
Hannes <hannes@octofox.de>
2022-12-16 18:57:46 +0100

    refactor: setProperty -> setPageProperty

--- src/Scribunto_ApiuntoLuaLibrary.php
+++ src/Scribunto_ApiuntoLuaLibrary.php
@@ -528,6 +528,6 @@ class Scribunto_ApiuntoLuaLibrary extends Scribunto_LuaLibraryBase {
 	 * @param AbstractRepository $repository
 	 */
 	private function writeCachePropertyKey( AbstractRepository $repository ): void {
-		$this->getParser()->getOutput()->setProperty( 'apiuntocache', $repository->makeCacheKey() );
+		$this->getParser()->getOutput()->setPageProperty( 'apiuntocache', $repository->makeCacheKey() );
 	}
 }
